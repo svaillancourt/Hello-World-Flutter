727c3f556999aa6ecf1baa58129bc756f927a41c
svaillancourt <svaillancourt@gmail.com>
2020-08-12 16:56:30 -0600

    added BizCard

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,52 @@
 import 'package:flutter/material.dart';
 
+class Wisdom extends StatefulWidget {
+  @override
+  _WisdomState createState() => _WisdomState();
+}
+
+int _index = 0;
+
+List quotes = [
+  "Never become so involved with something that it blinds you. Never forget where you from; someone will remind you. – DMX, It’s On",
+  "Identity is a prison you can never escape, but the way to redeem your past is not to run from it, but to try to understand it, and use it as a foundation to grow. – Jay Z",
+  "Jump regardless of the consequence, cause even on the night of the apocalypse, everybody’s an optimist. – Tonedeff, Optimist",
+  "The purpose of life is a life with a purpose. So, I’d rather die for a cause, than live a life that is worthless. – Immortal Technique",
+  "Rosa Parks sat so Martin Luther could walk. Martin Luther walked so Barack Obama could run. Barack Obama ran so all the children could fly. So I’ma spread my wings, you can meet me in the sky. – Jay Z, My President Is Black",
+  "My whole thing is to inspire, to better people, to better myself forever in this thing that we call rap, this thing that we call hip hop. – Kendrick Lamar",
+  "Even though you’re fed up, you got to keep ya head up. – 2Pac, Keep Ya Head Up",
+  "No law’s gonna change us, we have to change us. – Macklemore and Ryan Lewis, Same Love",
+  "It’s only human to express the way you really feel, but that same humanity is my Achilles’ heel. A leopard can’t change his spots and never will; So, I’m forever I’ll, now I can never chill. – Black Thought",
+  "And the land of the free is full of free loaders, leave us dead in the street to be your organ donors. – Joey Bada$$, Land of the Free",
+];
+
+class _WisdomState extends State<Wisdom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(quotes[_index % quotes.length]),
+            FlatButton.icon(
+                onPressed: _showQuote,
+                icon: Icon(Icons.music_note),
+                label: Text("Inspire with a verse!"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showQuote() {
+    // increment our index/counter by 1
+    setState(() {
+      _index += 1;
+    });
+  }
+}
+
 class BizCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
