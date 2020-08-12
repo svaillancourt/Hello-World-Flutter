5c2b19bc24d549d4395a919ba0d935f9f9d4d1d6
svaillancourt <svaillancourt@gmail.com>
2020-08-12 15:53:01 -0600

    added BizCard

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,6 @@ import 'ui/home.dart';
 
 void main() {
   runApp(new MaterialApp(
-    home: ScaffoldExample(),
+    home: BizCard(),
   ));
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,72 @@
 import 'package:flutter/material.dart';
 
+class BizCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BizCard"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            _getCard(),
+            _getAvatar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width: 350,
+      height: 200,
+      margin: EdgeInsets.all(50),
+      decoration: BoxDecoration(
+          color: Colors.pinkAccent, borderRadius: BorderRadius.circular(14.5)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Stephane Vaillancourt",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800),
+          ),
+          Icon(Icons.web_asset),
+          Text(
+            "TheOrganicBox.ca",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.person_outline),
+              Text("@StepCrazy27",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800))
+            ], // <Widget>[]
+          )
+        ], // <Widget>[]
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          border: Border.all(color: Colors.lightBlue, width: 5.2),
+          image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/id/237/200/300"),
+              fit: BoxFit.cover)),
+    );
+  }
+}
+
 class ScaffoldExample extends StatelessWidget {
   @override
   _tapButton() {
